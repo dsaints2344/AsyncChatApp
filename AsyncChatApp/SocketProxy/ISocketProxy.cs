50af70d4953decbe07e0d6ef494ec170b2917980
dsantos234 <ddelossantosmateo@gmail.com>
2019-09-17 16:24:05 -0400

    Starting to add projects and mock architecture

---
+++ AsyncChatApp/SocketProxy/ISocketProxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketProxy
+{
+    public interface ISocketProxy
+    {
+        void Bind(EndPoint localEP);
+        void Listen(int backlog);
+        IAsyncResult BeginAccept(AsyncCallback callback, object state);
+        Socket EndAccept(out byte[] buffer, IAsyncResult asyncResult);
+    }
+}
